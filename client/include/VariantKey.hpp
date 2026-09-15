7f7f03a9b9b08d9b327eb0dec12c40d94947acbe
Fuchs <fuchstraumer@gmail.com>
2026-09-15 08:52:53 -0700

    Add `INVALID_VARIANT` and `!` operator to `VariantKey`
    Simplifies use, and means we can return variant keys instead of something like a wrapper optional or pointer etc etc

--- client/include/VariantKey.hpp
+++ client/include/VariantKey.hpp
@@ -11,6 +11,13 @@ namespace lodestone
 // raw integers where a callsite expects a key: we'll have to cast to underlying type for packed output
 // or storage, but that's fine and clear as a boundary between strong typing and low-level representation.
 enum class VariantKey : uint64_t {};
+// set all bits to 1, since if we ever manage to hit that we're already in an invalid state
+// add operator! so functions returning variant keys can just do if (!resultKey)
+constexpr static VariantKey INVALID_VARIANT = static_cast<VariantKey>(0xFFFFFFFFFFFFFFFF);
+constexpr bool operator!(VariantKey key) noexcept
+{
+    return key == INVALID_VARIANT;
+}
 
 // This uses radix indices to avoid any Slang or Permutation includes - but shares common logic for
 // packing and unpacking the VariantKey from its underlying inputs that generate it. Used to make sure
